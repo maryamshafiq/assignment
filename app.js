8391bb857fbdd13af31057690ba83ecb638b0b94
maryamshafiq <maryam.sh2478@gmail.com>
2022-04-16 00:53:02 +0500

    work complete

---
+++ app.js
@@ -0,0 +1,183 @@
+// chapter 1/
+// Answer # 1
+
+// alert("Error! Please enter a valid Password.");
+
+// Answer # 2
+
+// var a = "Welcome to JS Land..."; 
+// var b = "\nHappy Coding!";
+// var c = (a+b);
+// alert(c);
+
+
+// Answer # 3
+
+// alert("Welcome to JS Land...");
+
+// Answer # 4
+
+// var x = "Happy Coding! \n  Prevent this page from creating additional dialogs.";
+// alert(x);
+
+// Answer # 5
+
+// console.log("Hello... I can run JS through my web browser's console");
+
+// Chapter # 2
+
+// Answer # 1
+
+// var username;
+
+// Answer # 2
+
+// var myName = "maryamShafiq";
+
+// Answer # 3
+
+// var message = "Hello World";
+// alert(message);
+
+// Answer # 4
+
+// var name = "Jhone Doe";
+// alert(name);
+
+// var age = "15 years old";
+// alert(age);
+
+// var skill = "Certified Mobile application Development";
+// alert(skill);
+
+// Answer # 5
+
+var x = "PIZZA \nPIZZ \nPIZ \nPI \nP";
+// alert(x);
+
+// Answer # 6
+
+// var a = "My email address is";
+// var b = " example@gmail.com";
+// var email = (a+b);
+// alert(email);
+
+// Answer # 7
+
+// var x = "I am trying to learn from the Book";
+// var y = " A Smarter way to learn JavaScript";
+// var book = (x+y);
+// alert(book);
+
+// Answer # 8
+
+// document.write("Yah! I can write HTML content through JavaScript.");
+
+// Answer # 9
+
+// var x ="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(x);
+
+// Chapter # 3
+
+// Answer # 1
+
+// var age = "I am 15 years old";
+// alert(age);
+
+// Answer # 2
+
+// var x = "You have visited this site ";
+// var y = "15 times";
+// var visited = (x+y);
+// alert(visited);
+
+// Answer # 3
+
+// var birthYear = "My birth year is 1997";
+// document.write(birthYear);
+
+// Answer # 4
+
+// var vname = "John Doe ";
+// var product = "ordered 15 T-Shirt(s) ";
+// var store = "on XYZ Clothing Store";
+// document.write(vname+product+store);
+
+// Chapter # 4
+
+// Answer # 1
+
+// var name,age,nationality;
+
+// Answer # 2
+
+// var name,age,nationality,product,store(legal names);
+// var 1stperson,alert,var,2sid,"name"(Illegal names);
+
+// Answer # 3
+
+// var a = "Rules for naming JS variables";
+// var b = "Variable names can only contain numbers,$ and _. For example: $my_1st variable";
+// var c = "Variable must begin with a letter, $ or _. For example $name, _name or name";
+// var d = "Variable names are case sensitive";
+// var e = "Variable names should not be JS keywords";
+// document.write(a+"<br>"+"<br>"+"<br>" +b+"<br>"+c+"<br>"+d+"<br>"+e);
+
+// Chapter # 5
+
+// Answer # 1
+
+// var a = 3;
+// var b = 5;
+// var c = (a+b);
+// document.write(c+"<br>");
+// document.write("The sum of 3 and 5 is 8");
+
+// Answer # 2
+
+// var a = 3;
+// var b = 5;
+// var c = (a-b);
+// document.write(c);
+
+// var a = 3;
+// var b = 5;
+// var c = (a*b);
+// document.write(c+"<br>");
+// document.write("The Multiplication of 3 and 5 is 15");
+
+// var a = 3;
+// var b = 5;
+// var c = (a/b);
+// document.write(c+"<br>");
+// document.write("The Division of 3 and 5 is 0.6");
+
+// var a = 5;
+// var b = 3;
+// var c = (a%b);
+// document.write(c+"<br>");
+// document.write("The remainder is 2");
+
+// Answer # 3
+
+var num;
+document.write("Value after variable declaration is undefined." +"<br>"+"<br>");
+var num = 5;
+document.write("Initial Value: 5"+"<br>");
+var newNum = num++;
+document.write(newNum+"<br>");
+// (first print than add that's why output is 5)
+document.write("Value after increment is: 6"+"<br>");
+var newNum = num++ +7;
+document.write(newNum+"<br>");
+document.write("Value after addition is: 13"+"<br>");
+var no = newNum--;
+document.write(no+"<br>");
+// (first print than substract that's why output is 13)
+document.write("Value after decrement is: 12"+"<br>");
+var value = no
+document.write(value+"<br>");
+
+
+
